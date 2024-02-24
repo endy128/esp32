57f57c5364c78482d5e62fb765481be0c79eb4ef
Neil Douglas <endy64@icloud.com>
2024-02-24 16:13:19 +0000

    added speaker playing function

--- washing/src/main.cpp
+++ washing/src/main.cpp
@@ -9,7 +9,12 @@
 // Replace with your own network credentials
 const char* ssid = SSID;
 const char* password = PASS;
- 
+
+// monitor for voltage on GPIO34
+const int potPin = 34;
+
+// variable for storing the potentiometer value
+int potValue = 0; 
  
 void ConnectedToAP_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
   Serial.println("Connected To The WiFi Network");
@@ -25,6 +30,12 @@ void WiFi_Disconnected_Handler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info
   // Attempt Re-Connection
   WiFi.begin(ssid, password);
 }
+
+char* signal_Speak_On() {
+  char* msg = new char[16];
+  strcpy(msg,"SPEAKER PLAYING");
+  return msg;
+}
  
 void setup() {
   Serial.begin(115200);
@@ -38,5 +49,14 @@ void setup() {
 }
  
 void loop() {
-  // Do Nothing
+  // Reading value
+  potValue = analogRead(potPin);
+  if (potValue > 4000) {
+    Serial.println(signal_Speak_On());
+  }
+  else {
+    Serial.print("GPIO34: ");
+    Serial.println(potValue);
+  }
+  delay(500);
 }
